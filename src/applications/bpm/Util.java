23dc3885e29d5b2c62a156956015787c7dd76b5f
Jakob Mass <jaks@ut.ee>
2019-06-19 11:44:52 +0300

    Small fixes - missing file and update import statement

--- src/applications/bpm/Util.java
+++ src/applications/bpm/Util.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static applications.model.AdaptiveMobileApplication.COMBUS_PENDING_FOG_REQS;
+import static applications.fog.AdaptiveMobileApplication.COMBUS_PENDING_FOG_REQS;
 
 public class Util {
     public static HashMap<String, String> PROCESSES =  new HashMap<>();
